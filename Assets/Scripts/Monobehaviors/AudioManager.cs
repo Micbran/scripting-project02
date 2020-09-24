c0117823ceeed0ec0ec75fed7dfa6f6db2ff3a0e
Micbran <micbran22@gmail.com>
2020-09-24 13:08:58 -0500

    Completed AudioManager portion.

---
+++ Assets/Scripts/Monobehaviors/AudioManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioManager : Singleton<AudioManager>
+{
+    private AudioSource audioSource;
+
+    [SerializeField] private AudioClip startingSong = null;
+
+    public override void Awake()
+    {
+        Instance = this;
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void Start()
+    {
+        this.PlaySong(startingSong);
+    }
+
+    public void PlaySong(AudioClip clip)
+    {
+        audioSource.clip = clip;
+        audioSource.Play();
+    }
+}
---
+++ Assets/Scripts/Monobehaviors/Level01Controller.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Level01Controller : MonoBehaviour
+{
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            LoadMainMenu();
+        }
+    }
+
+    private void LoadMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
